6dde99f241c04d946b29e32d7fc029f8424492cd
Mindaugas <gelumind@gmail.com>
2020-10-29 19:36:58 +0200

    Book entity

---
+++ src/main/java/lt/gelumind/personallibrary/model/Books.java
@@ -0,0 +1,20 @@
+package lt.gelumind.personallibrary.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Books {
+
+    private List<Book> bookList;
+
+    public List<Book> getBookList() {
+        if (bookList == null) {
+            bookList = new ArrayList<>();
+        }
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+}
